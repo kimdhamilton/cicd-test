c484f37640da20ff465e55e73d3a7c6b54df00e7
kimdhamilton <kimdhamilton@gmail.com>
2020-01-27 12:11:51 -0800

    updates

--- generate-html.sh
+++ generate-html.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 DATE=2020-01-21
-DIRECTORY=../$DATE
+DIRECTORY=$DATE
+
+# cd scribe-tool
 
 echo "Generating minutes for $DATE"
 # Generate minutes
-$NODE_COMMAND scribe-tool/index.js -d $DIRECTORY -m -i
+node scribe-tool/index.js -d $DIRECTORY -m -i
 
 MESSAGE="Add text minutes for $DATE telecon."
 
-git add $DIRECTORY/index.html
+git add $DIRECTORY/index.html index.html
 git commit $DIRECTORY/index.html index.html -m "$MESSAGE"
 git push
 
