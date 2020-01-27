9db03e27b91a22b99729eb9d35ef2dac7d5636d0
kimdhamilton <kimdhamilton@gmail.com>
2020-01-27 14:59:07 -0800

    fixes

--- generate-html.sh
+++ generate-html.sh
@@ -2,18 +2,17 @@
 
 DATE=2020-01-21
 
-if [[ -f "$DATE/irc.log" && ! -f "$DATE/index.html" ]];    
-	DIRECTORY=$DATE
+if [[ -f "$DATE/irc.log" && ! -f "$DATE/index.html" ]]; then  
+    DIRECTORY=$DATE
 
-	echo "Generating minutes for $DATE"
-	# Generate minutes
-	node scribe-tool/index.js -d $DIRECTORY -m -i
+    echo "Generating minutes for $DATE"
+    # Generate minutes
+    node scribe-tool/index.js -d $DIRECTORY -m -i
+    MESSAGE="Add text minutes for $DATE telecon."
 
-	MESSAGE="Add text minutes for $DATE telecon."
-
-	git add $DIRECTORY/index.html index.html
-	git commit $DIRECTORY/index.html index.html -m "$MESSAGE [ci skip]"
-	git push origin master
+    git add $DIRECTORY/index.html index.html
+    git commit $DIRECTORY/index.html index.html -m "$MESSAGE [ci skip]"
+    git push origin master
 else
-  echo "nothing to do"
+    echo "nothing to do"
 fi
