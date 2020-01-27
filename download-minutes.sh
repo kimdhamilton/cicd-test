5932907d5959fc8c4dd8bd683f197b7d623f025e
kimdhamilton <kimdhamilton@gmail.com>
2020-01-27 14:44:26 -0800

    update scripts

--- download-minutes.sh
+++ download-minutes.sh
@@ -2,7 +2,7 @@
 
 DATE=2020-01-21
 
-if [ ! -f $DATE/irc-raw.log ]; then
+if [ ! -f "$DATE/irc-raw.log" ]; then
     mkdir -p $DATE
     echo "....Downloading IRC logs for $DATE..."
     curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $DATE/irc-raw.log
@@ -12,9 +12,6 @@ if [ ! -f $DATE/irc-raw.log ]; then
     MESSAGE="Add raw log for $DATE telecon."
     git commit $DATE/irc-raw.log -m "$MESSAGE [ci skip]"
     git push origin master
+else
+  echo "nothing to do"
 fi
-
-
-
-
-
--- generate-html.sh
+++ generate-html.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 DATE=2020-01-21
-DIRECTORY=$DATE
 
-# cd scribe-tool
+if [[ -f "$DATE/irc.log" && ! -f "$DATE/index.html" ]];    
+	DIRECTORY=$DATE
 
-echo "Generating minutes for $DATE"
-# Generate minutes
-node scribe-tool/index.js -d $DIRECTORY -m -i
+	echo "Generating minutes for $DATE"
+	# Generate minutes
+	node scribe-tool/index.js -d $DIRECTORY -m -i
 
-MESSAGE="Add text minutes for $DATE telecon."
+	MESSAGE="Add text minutes for $DATE telecon."
 
-git add $DIRECTORY/index.html index.html
-git commit $DIRECTORY/index.html index.html -m "$MESSAGE"
-git push origin master
-
+	git add $DIRECTORY/index.html index.html
+	git commit $DIRECTORY/index.html index.html -m "$MESSAGE [ci skip]"
+	git push origin master
+else
+  echo "nothing to do"
+fi
