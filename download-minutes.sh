d7aa565d32dafe5cdac4fa03ae68d6de6db70094
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2020-01-26 17:28:44 -0800

    Update download-minutes.sh

--- download-minutes.sh
+++ download-minutes.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
 DATE=2020-01-21
 mkdir -p $DATE
 echo "....Downloading IRC logs for $DATE..."
 curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $DATE/irc-raw.log
+
+# commit raw logs
+git add $DATE/irc-raw.log
+MESSAGE="Add raw log for $DATE telecon."
+git commit $DATE/irc-raw.log -m "$MESSAGE"
+git push
+
