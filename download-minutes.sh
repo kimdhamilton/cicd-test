2ddb0af1a582b099aca2fa97e866a378538d0cf9
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2020-01-27 12:19:21 -0800

    Update download-minutes.sh

--- download-minutes.sh
+++ download-minutes.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 DATE=2020-01-21
-mkdir -p $DATE
-echo "....Downloading IRC logs for $DATE..."
-curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $DATE/irc-raw.log
 
-# commit raw logs
-git add $DATE/irc-raw.log
-MESSAGE="Add raw log for $DATE telecon."
-git commit $DATE/irc-raw.log -m "$MESSAGE [ci skip]"
-git push origin master
+if [ ! -f $DATE/irc-raw.log ]; then
+    mkdir -p $DATE
+    echo "....Downloading IRC logs for $DATE..."
+    curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $DATE/irc-raw.log
+    
+    # commit raw logs
+    git add $DATE/irc-raw.log
+    MESSAGE="Add raw log for $DATE telecon."
+    git commit $DATE/irc-raw.log -m "$MESSAGE [ci skip]"
+    git push origin master
+fi
+
+
+
+
 
