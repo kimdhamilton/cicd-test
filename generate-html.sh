4176ba8d2ca5aa9a485db0d65478b9ef238f1b1d
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2020-01-27 17:17:59 -0800

    Update generate-html.sh

--- generate-html.sh
+++ generate-html.sh
@@ -6,11 +6,6 @@ if [[ -f "$DATE/irc.log" && ! -f "$DATE/index.html" ]]; then
     echo "Generating minutes for $DATE"
     # Generate minutes
     node scribe-tool/index.js -d $DIRECTORY -m -i
-    MESSAGE="Add text minutes for $DATE telecon."
-
-    git add $DIRECTORY/index.html index.html
-    git commit $DIRECTORY/index.html index.html -m "$MESSAGE [ci skip]"
-    git push origin master
 else
     echo "nothing to do"
     exit 1
