d531ba9db96f8920db1189f5faa721e0fe794762
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2020-01-27 17:15:53 -0800

    Update download-minutes.sh

--- download-minutes.sh
+++ download-minutes.sh
@@ -4,12 +4,6 @@ if [ ! -f "$DATE/irc-raw.log" ]; then
     mkdir -p $DATE
     echo "....Downloading IRC logs for $DATE..."
     curl -# "https://w3c-ccg.s3.digitalbazaar.com/minutes/$DATE-irc.log" > $DATE/irc-raw.log
-    
-    # commit raw logs
-    git add $DATE/irc-raw.log
-    MESSAGE="Add raw log for $DATE telecon."
-    git commit $DATE/irc-raw.log -m "$MESSAGE [ci skip]"
-    git push
 else
   echo "nothing to do"
   exit 1
